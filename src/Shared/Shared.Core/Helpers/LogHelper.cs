2a17fd7e2dc2221e442468a20d3404627ece4a0b
wesleybertipaglia <wesleybertipaglia@gmail.com>
2025-09-13 16:51:31 -0300

    feat: add LogHelper class for structured logging functionality

---
+++ src/Shared/Shared.Core/Helpers/LogHelper.cs
@@ -0,0 +1,38 @@
+using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Logging;
+
+namespace Shared.Core.Helpers;
+
+public static class LogHelper
+{
+    public static void LogInfo<T>(
+        ILogger<T> logger,
+        string message,
+        object[]? args = null,
+        [CallerMemberName] string methodName = "")
+    {
+        var tag = $"({typeof(T).Name}_{methodName})";
+        logger.LogInformation(tag + " " + message, args ?? []);
+    }
+
+    public static void LogWarning<T>(
+        ILogger<T> logger,
+        string message,
+        object[]? args = null,
+        [CallerMemberName] string methodName = "")
+    {
+        var tag = $"({typeof(T).Name}_{methodName})";
+        logger.LogWarning(tag + " " + message, args ?? []);
+    }
+
+    public static void LogError<T>(
+        ILogger<T> logger,
+        Exception ex,
+        string message,
+        object[]? args = null,
+        [CallerMemberName] string methodName = "")
+    {
+        var tag = $"({typeof(T).Name}_{methodName})";
+        logger.LogError(ex, tag + " " + message, args ?? []);
+    }
+}

ca9c96a9463c3165fede01fceb018105e91368dc
wesleybertipaglia <wesleybertipaglia@gmail.com>
2025-09-14 15:05:32 -0300

    feat: add GetVerificationCodeKeyByUserIdAndType method to CacheKeysHelper for verification code retrieval

--- src/Shared/Shared.Core/Helpers/CacheKeysHelper.cs
+++ src/Shared/Shared.Core/Helpers/CacheKeysHelper.cs
@@ -4,4 +4,5 @@ public static class CacheKeysHelper
 {
     public static string GetUserIdKey(Guid id) => $"user:id:{id}";
     public static string GetUserEmailKey(string email) => $"user:email:{email}";
+    public static string GetVerificationCodeKeyByUserIdAndType(Guid userId, string type) => $"verification:user:id:{userId}:type:{type}";
 }

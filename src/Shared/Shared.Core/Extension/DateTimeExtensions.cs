274403c6f374baa01004d63df751fa20a0f89e29
wesleybertipaglia <wesleybertipaglia@gmail.com>
2025-09-14 15:05:27 -0300

    feat: add DateTimeExtensions class with methods for time span calculations

---
+++ src/Shared/Shared.Core/Extension/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+namespace Shared.Core.Extensions;
+
+public static class DateTimeExtensions
+{
+    public static TimeSpan ToTimeSpanUntil(this DateTime futureTime)
+    {
+        
+        var duration = futureTime - DateTime.UtcNow;
+        return duration > TimeSpan.Zero ? duration : TimeSpan.Zero;
+    }
+
+    public static TimeSpan ToExpirationTimeSpan(this DateTime expiresAt)
+    {
+        return expiresAt.ToTimeSpanUntil();
+    }
+}
